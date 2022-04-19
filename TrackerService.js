017640b346811c3c38e9b42f1ec50f87e33b65a7
yvan.kombou <yvan.kombou@kaeyros-analytics.com>
2022-04-19 18:14:55 +0200

    =add server information

--- TrackerService.js
+++ TrackerService.js
@@ -2,12 +2,6 @@ var TrackerService = TrackerService || {};
 
 
 TrackerService.getTracker = () => {
-
-  configCookiesDisabled = false
-  configCookieNamePrefix = '_pk_',
-    configTrackerSiteId = '',
-    domainHash = '',
-
     /**
      * Get the visitor information (from first party cookie)
      *
@@ -18,98 +12,41 @@ TrackerService.getTracker = () => {
       return [true, "ID8OJ", 0, new Date(), new Date() ]
     }
 
-  TrackerService.trackEvent = (cat, action, name, value) => {
-    console.log(cat, action, name, value)
-  }
+    TrackerService.trackEvent = (cat, action, name, value) => {
+      clearTimeout(TID);
 
-  TrackerService.setCustomDimension = (index, value) => {
-    console.log(index, value)
-  }
+      TID = setTimeout(()=> {
+        var data = e.detail;
+        linkData.push(data);
 
-  getCookie = (cookieName) => {
-    if (configCookiesDisabled) {
-      return 0;
+      }, 100)
+
+      setInterval(() => {
+        if (!linkData.length) {
+          return;
+        }
+
+        const data = {cat: cat, action: action, name: name, value: value};
+        console.log(data)
+
+        fetch('http://192.168.1.11:5000/api/link-data', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        }).then(response => response.json()).then(data => {
+          console.log('Success:', data);
+        }).catch((error) => {
+          console.error('Error:', error);
+        });
+      }, 1000)
     }
 
-    getCookieName = (baseName) => {
-      return configCookieNamePrefix + baseName + '.' + configTrackerSiteId + '.' + domainHash;
+    TrackerService.setCustomDimension = (index, value) => {
+      console.log(index, value)
     }
-
-
-
-    var cookiePattern = new RegExp('(^|;)[ ]*' + cookieName + '=([^;]*)'),
-      cookieMatch = cookiePattern.exec(documentAlias.cookie);
-
-    return cookieMatch ? decodeWrapper(cookieMatch[2]) : 0;
-  }
-
-  setVisitorIdCookie = (visitorIdCookieValues) =>{
-    if(!configTrackerSiteId) {
-      // when called before Site ID was set
-      return;
-    }
-
-    var now = new Date(),
-      nowTs = Math.round(now.getTime() / 1000);
-
-    if(!isDefined(visitorIdCookieValues)) {
-      visitorIdCookieValues = getValuesFromVisitorIdCookie();
-    }
-
-    var cookieValue = visitorIdCookieValues.uuid + '.' +
-      visitorIdCookieValues.createTs + '.';
-
-    setCookie(getCookieName('id'),
-      cookieValue, getRemainingVisitorCookieTimeout(),
-      configCookiePath, configCookieDomain, configCookieIsSecure, configCookieSameSite);
-  }
 
 
   return TrackerService;
 }
-
-
-
-var ajax;
-
-var linkData = [];
-var TID = null;
-/*
-Log that we received the message.
-which we read from the message.
-*/
-function notify(e) {
-  clearTimeout(TID);
-
-  TID = setTimeout(()=> {
-    var data = e.detail;
-    linkData.push(data);
-
-  }, 100)
-
-  setInterval(() => {
-    if (!linkData.length) {
-      return;
-    }
-
-    const data = { links: linkData };
-
-    fetch('http://192.168.1.11:5000/api/link-data', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    }).then(response => response.json()).then(data => {
-      linkData = []
-      console.log('Success:', data);
-    }).catch((error) => {
-      console.error('Error:', error);
-    });
-  }, 1000)
-}
-
-/*
-Assign `notify()` as a listener to messages from the content script.
-*/
-// window.addEventListener('link', notify);
